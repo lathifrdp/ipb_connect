5e1df543276812ce55e18758f2aa067d1fd97b4b
lathifrdp <lathifrdp@gmail.com>
2018-10-14 00:19:48 +0700

    load data event

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/event/EventFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/event/EventFragment.java
@@ -3,14 +3,44 @@ package com.example.lathifrdp.demoapp.fragment.event;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lathifrdp.demoapp.R;
+import com.example.lathifrdp.demoapp.adapter.EventList;
+import com.example.lathifrdp.demoapp.api.ApiClient;
+import com.example.lathifrdp.demoapp.api.ApiInterface;
+import com.example.lathifrdp.demoapp.helper.SessionManager;
+import com.example.lathifrdp.demoapp.model.Event;
+import com.example.lathifrdp.demoapp.response.EventResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EventFragment extends Fragment{
+
+    private String title;
+    TextView tv;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ListView listView;
+    EventList adapter;
+    ApiInterface apiService;
+    SessionManager sessionManager;
+    private int page = 1;
+    private boolean isRefresh = false;
+    private List<Event> listEvent;
+    private int limitpage=0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,11 +51,78 @@ public class EventFragment extends Fragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.eventFragment).setOnClickListener(new View.OnClickListener() {
+        listView=(ListView)getView().findViewById(R.id.listEvent);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipeToRefresh);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+
+        sessionManager = new SessionManager(getActivity());
+
+        loadDataEvent();
+
+        listEvent = new ArrayList<>();
+        adapter= new EventList(listEvent,getActivity());
+        listView.setAdapter(adapter);
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(),"Event Fragment",Toast.LENGTH_SHORT).show();
+            public void onRefresh() {
+                isRefresh = true;
+                //page=1;
+                loadDataEvent();
+            }
+        });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if(scrollState == SCROLL_STATE_IDLE && listView.getLastVisiblePosition() ==
+                        adapter.getSize() - 1){
+                    if(page<limitpage+1) {
+                        loadDataEvent();
+                        Toast.makeText(getActivity(), "lanjut " + page, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                //we don't need this method, so we leave it empty
+            }
+        });
+
+    }
+
+    private void loadDataEvent(){
+
+        //spinner = (Spinner) getView().findViewById(R.id.prodiFragment);
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+        //ApiService apiService = ApiClient.getClient().create(ApiService.class);
+
+        Call<EventResponse> call = apiService.getEvent("JWT "+ sessionManager.getKeyToken(),page);
+        call.enqueue(new Callback<EventResponse>() {
+            @Override
+            public void onResponse(Call<EventResponse> call, Response<EventResponse> response) {
+                mSwipeRefreshLayout.setRefreshing(false);
+                if (response.isSuccessful()) {
+                    if(isRefresh) adapter.setList(response.body().getEvent());
+                    else adapter.addList(response.body().getEvent());
+                    isRefresh = false;
+                    adapter.notifyDataSetChanged();
+
+                    int total = response.body().getTotal();
+                    int limit = response.body().getLimit();
+                    limitpage = (int)Math.ceil((double)total/limit);
+                    page++;
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<EventResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "gagal", Toast.LENGTH_SHORT).show();
+                mSwipeRefreshLayout.setRefreshing(false);
             }
         });
     }
+
 }
