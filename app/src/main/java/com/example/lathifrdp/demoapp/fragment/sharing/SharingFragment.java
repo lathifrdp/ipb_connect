9b50b760e0da6419f9667e4f48157de9628bc36e
lathifrdp <lathifrdp@gmail.com>
2018-10-30 09:31:25 +0700

    tambah getview viewpager, tablayout

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/sharing/SharingFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/sharing/SharingFragment.java
@@ -2,15 +2,21 @@ package com.example.lathifrdp.demoapp.fragment.sharing;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+//import android.widget.Toolbar;
 
 import com.example.lathifrdp.demoapp.R;
+import com.example.lathifrdp.demoapp.adapter.ViewPagerAdapter;
 
-public class SharingFragment extends Fragment{
+public class SharingFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,11 +27,22 @@ public class SharingFragment extends Fragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.sharingFragment).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(),"Sharing Fragment",Toast.LENGTH_SHORT).show();
-            }
-        });
+        Toolbar toolbar = getView().findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewPager viewPager = getView().findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = getView().findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(PageFragment.newInstance("Satu"), "Satu");
+        adapter.addFragment(PageFragment.newInstance("Dua"), "Dua");
+        adapter.addFragment(PageFragment.newInstance("Tiga"), "Tiga");
+        viewPager.setAdapter(adapter);
     }
 }
