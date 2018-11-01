d6d5089f170bd76a946e8963d982ec579b2c99a1
lathifrdp <lathifrdp@gmail.com>
2018-11-01 17:20:27 +0700

    create KesehatanFragment

---
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/sharing/KesehatanFragment.java
@@ -0,0 +1,39 @@
+package com.example.lathifrdp.demoapp.fragment.sharing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.lathifrdp.demoapp.R;
+
+public class KesehatanFragment extends Fragment {
+    public static KesehatanFragment newInstance() {
+
+        Bundle args = new Bundle();
+        KesehatanFragment fragment = new KesehatanFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kesehatan,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.kesehatanFragment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Kesehatan Fragment",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
