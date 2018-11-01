b62f1044d7b6d05871a8722a65812bae2eff5b12
lathifrdp <lathifrdp@gmail.com>
2018-11-01 21:26:13 +0700

    komen attribut

--- app/src/main/java/com/example/lathifrdp/demoapp/model/KnowledgeSharing.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/model/KnowledgeSharing.java
@@ -9,57 +9,57 @@ public class KnowledgeSharing {
     @SerializedName("title")
     private String title;
 
-    @SerializedName("description")
-    private String description;
+//    @SerializedName("description")
+//    private String description;
 
-    @SerializedName("category")
-    private KnowledgeSharingCategory category;
+//    @SerializedName("category")
+//    private KnowledgeSharingCategory category;
 
     @SerializedName("cover")
     private String cover;
 
-    @SerializedName("file")
-    private String file;
+//    @SerializedName("file")
+//    private String file;
 
     @SerializedName("fileSize")
     private String fileSize;
 
     @SerializedName("totalSlide")
-    private int totalSlide;
+    private String totalSlide;
 
     @SerializedName("totalLike")
-    private int totalLike;
+    private String totalLike;
 
-    @SerializedName("totalComment")
-    private int totalComment;
+//    @SerializedName("totalComment")
+//    private String totalComment;
 
-    @SerializedName("likers")
-    private Liker liker;
+//    @SerializedName("likers")
+//    private Liker liker;
+//
+//    @SerializedName("comments")
+//    private Comment comment;
+//
+//    @SerializedName("bookmarks")
+//    private Bookmark bookmark;
 
-    @SerializedName("comments")
-    private Comment comment;
-
-    @SerializedName("bookmarks")
-    private Bookmark bookmark;
-
-    @SerializedName("createdBy")
-    private String createdBy;
+//    @SerializedName("createdBy")
+//    private String createdBy;
 
     @SerializedName("created")
     private String created;
 
-    public KnowledgeSharing(String id, String title, String description, String cover, String file, String fileSize, int totalSlide, int totalLike, int totalComment, String created, String createdBy) {
+    public KnowledgeSharing(String id, String title, String description, String cover, String file, String fileSize, String totalSlide, String totalLike, String totalComment, String created, String createdBy) {
         this.id = id;
         this.title = title;
-        this.description = description;
+        //this.description = description;
         this.cover = cover;
-        this.file = file;
+        //this.file = file;
         this.fileSize = fileSize;
         this.totalSlide = totalSlide;
         this.totalLike = totalLike;
-        this.totalComment = totalComment;
+        //this.totalComment = totalComment;
         this.created = created;
-        this.createdBy = createdBy;
+        //this.createdBy = createdBy;
     }
 
     public String getId() {
@@ -78,13 +78,13 @@ public class KnowledgeSharing {
         this.title = title;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 
     public String getCover() {
         return cover;
@@ -94,13 +94,13 @@ public class KnowledgeSharing {
         this.cover = cover;
     }
 
-    public String getFile() {
-        return file;
-    }
-
-    public void setFile(String file) {
-        this.file = file;
-    }
+//    public String getFile() {
+//        return file;
+//    }
+//
+//    public void setFile(String file) {
+//        this.file = file;
+//    }
 
     public String getFileSize() {
         return fileSize;
@@ -110,59 +110,59 @@ public class KnowledgeSharing {
         this.fileSize = fileSize;
     }
 
-    public KnowledgeSharingCategory getCategory() {
-        return category;
-    }
+//    public KnowledgeSharingCategory getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(KnowledgeSharingCategory category) {
+//        this.category = category;
+//    }
+//
+//    public Liker getLiker() {
+//        return liker;
+//    }
+//
+//    public void setLiker(Liker liker) {
+//        this.liker = liker;
+//    }
+//
+//    public Comment getComment() {
+//        return comment;
+//    }
+//
+//    public void setComment(Comment comment) {
+//        this.comment = comment;
+//    }
+//
+//    public Bookmark getBookmark() {
+//        return bookmark;
+//    }
+//
+//    public void setBookmark(Bookmark bookmark) {
+//        this.bookmark = bookmark;
+//    }
 
-    public void setCategory(KnowledgeSharingCategory category) {
-        this.category = category;
-    }
-
-    public Liker getLiker() {
-        return liker;
-    }
-
-    public void setLiker(Liker liker) {
-        this.liker = liker;
-    }
-
-    public Comment getComment() {
-        return comment;
-    }
-
-    public void setComment(Comment comment) {
-        this.comment = comment;
-    }
-
-    public Bookmark getBookmark() {
-        return bookmark;
-    }
-
-    public void setBookmark(Bookmark bookmark) {
-        this.bookmark = bookmark;
-    }
-
-    public int getTotalLike() {
+    public String getTotalLike() {
         return totalLike;
     }
 
-    public void setTotalLike(int totalLike) {
+    public void setTotalLike(String totalLike) {
         this.totalLike = totalLike;
     }
 
-    public int getTotalComment() {
-        return totalComment;
-    }
+//    public String getTotalComment() {
+//        return totalComment;
+//    }
+//
+//    public void setTotalComment(String totalComment) {
+//        this.totalComment = totalComment;
+//    }
 
-    public void setTotalComment(int totalComment) {
-        this.totalComment = totalComment;
-    }
-
-    public int getTotalSlide() {
+    public String getTotalSlide() {
         return totalSlide;
     }
 
-    public void setTotalSlide(int totalSlide) {
+    public void setTotalSlide(String totalSlide) {
         this.totalSlide = totalSlide;
     }
 
@@ -174,11 +174,11 @@ public class KnowledgeSharing {
         this.created = created;
     }
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
+//    public String getCreatedBy() {
+//        return createdBy;
+//    }
+//
+//    public void setCreatedBy(String createdBy) {
+//        this.createdBy = createdBy;
+//    }
 }
