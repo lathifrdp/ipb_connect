7dd178b69bbf1c7cc8ab9646aed255d8e905659d
lathifrdp <lathifrdp@gmail.com>
2018-11-02 00:14:43 +0700

    rename

--- app/src/main/java/com/example/lathifrdp/demoapp/adapter/SharingAdapter.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/adapter/SharingAdapter.java
@@ -1,7 +1,6 @@
 package com.example.lathifrdp.demoapp.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,36 +15,35 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder>{
-    private List<KnowledgeSharing> movies;
+public class SharingAdapter extends RecyclerView.Adapter<SharingAdapter.ViewHolder>{
+    private List<KnowledgeSharing> knowledgeSharings;
     private Context context;
     // private Movie movie;
 
-    public MoviesAdapter(Context applicationContext, List<KnowledgeSharing> movieArrayList) {
+    public SharingAdapter(Context applicationContext, List<KnowledgeSharing> sharingArrayList) {
         this.context =applicationContext;
-        this.movies=movieArrayList;
+        this.knowledgeSharings=sharingArrayList;
     }
 
     @Override
-    public MoviesAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_movie, viewGroup, false);
-
-
+    public SharingAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_sharing, viewGroup, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(MoviesAdapter.ViewHolder viewHolder, int i) {
-        viewHolder.title.setText(movies.get(i).getTitle());
-        viewHolder.actors.setText(movies.get(i).getTotalSlide());
-        viewHolder.year.setText(movies.get(i).getCreated());
+    public void onBindViewHolder(SharingAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(knowledgeSharings.get(i).getTitle());
+        viewHolder.slide.setText(knowledgeSharings.get(i).getTotalSlide()+" Slide");
+        viewHolder.created.setText(knowledgeSharings.get(i).getCreated());
+        viewHolder.filesize.setText(knowledgeSharings.get(i).getFileSize());
 
         // This is how we use Picasso to load images from the internet.
 //        Picasso.with(context)
 //                .load(movies.get(i).getCover())
 //                .placeholder(R.drawable.load)
 //                .into(viewHolder.imageView);
-        String url = "http://182.23.70.28:3501/uploads/knowledgesharing/"+movies.get(i).getCover();
+        String url = "http://182.23.70.28:3501/uploads/knowledgesharing/"+knowledgeSharings.get(i).getCover();
         Picasso.get()
                 .load(url)
                 .placeholder(R.drawable.logoipb)
@@ -56,18 +54,19 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
     }
     @Override
     public int getItemCount() {
-        return movies.size();
+        return knowledgeSharings.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView title, year, actors;
+        private TextView title, slide, created, filesize;
         private ImageView imageView;
 
         public ViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.title);
-            year = (TextView) view.findViewById(R.id.year);
-            actors = (TextView) view.findViewById(R.id.actors);
+            slide = (TextView) view.findViewById(R.id.slide);
+            created = (TextView) view.findViewById(R.id.created);
+            filesize = (TextView) view.findViewById(R.id.filesize);
             imageView= (ImageView) view.findViewById(R.id.cover);
 
             // on item click
@@ -78,7 +77,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
                     int pos = getAdapterPosition();
                     // check if item still exists
                     if(pos != RecyclerView.NO_POSITION){
-                        KnowledgeSharing clickedDataItem = movies.get(pos);
+                        KnowledgeSharing clickedDataItem = knowledgeSharings.get(pos);
 //                        Intent intent = new Intent(context, .class);
 //                        intent.putExtra("movie_title", movies.get(pos).getTitle());
 //                        intent.putExtra("movie_actors", movies.get(pos).getActors());
@@ -98,14 +97,14 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
     /* Within the RecyclerView.Adapter class */
     // Clean all elements of the recycler
     public void clear() {
-        movies.clear();
+        knowledgeSharings.clear();
         notifyDataSetChanged();
     }
 
     //RecyclerView mRecycler;
     // Add a list of ites
     public void addAll(int position, List<KnowledgeSharing> mov) {
-        movies.addAll(0,mov);
+        knowledgeSharings.addAll(0,mov);
         notifyItemInserted(0);
         //mRecycler.smoothScrollToPosition(0);
         notifyDataSetChanged();
