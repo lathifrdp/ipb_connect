a3ad55121487014cc11d0cbbf630f4dc86b8fba3
lathifrdp <lathifrdp@gmail.com>
2018-11-08 16:30:00 +0700

    onclick memories to detail

--- app/src/main/java/com/example/lathifrdp/demoapp/adapter/MemoriesList.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/adapter/MemoriesList.java
@@ -1,5 +1,11 @@
 package com.example.lathifrdp.demoapp.adapter;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +13,17 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lathifrdp.demoapp.R;
+import com.example.lathifrdp.demoapp.fragment.memories.DetailMemoriesFragment;
 import com.example.lathifrdp.demoapp.model.Memory;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class MemoriesList extends RecyclerView.Adapter<MemoriesList.MyViewHolder>{
-    private List<Memory> moviesList;
+    private List<Memory> memoryList;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView caption;
@@ -30,7 +38,7 @@ public class MemoriesList extends RecyclerView.Adapter<MemoriesList.MyViewHolder
 
 
     public MemoriesList(List<Memory> moviesList) {
-        this.moviesList = moviesList;
+        this.memoryList = moviesList;
     }
 
     @Override
@@ -43,16 +51,39 @@ public class MemoriesList extends RecyclerView.Adapter<MemoriesList.MyViewHolder
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Memory movie = moviesList.get(position);
-        holder.caption.setText(movie.getCaption());
+        final Memory memory = memoryList.get(position);
+        holder.caption.setText(memory.getCaption());
         holder.photo.setImageBitmap(null);
         Picasso.get().cancelRequest(holder.photo);
-        String url = "http://api.ipbconnect.cs.ipb.ac.id/uploads/memory/"+movie.getPhoto();
+        String url = "http://api.ipbconnect.cs.ipb.ac.id/uploads/memory/"+memory.getPhoto();
         Picasso.get().load(url).placeholder(R.drawable.logoipb).error(R.drawable.alumni2).into(holder.photo);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bundle bundle;
+                bundle = new Bundle();
+                Fragment newFragment = null;
+                newFragment = new DetailMemoriesFragment();
+
+                bundle.putString("id_memory",memory.getId());
+                newFragment.setArguments(bundle);
+
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                FragmentManager fragmentManager = activity.getSupportFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.screen_area, newFragment);
+                ft.addToBackStack(null);
+                ft.commit();
+
+                Toast.makeText(view.getContext(), memory.getId() + " " + memory.getCaption(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return moviesList.size();
+        return memoryList.size();
     }
 }
