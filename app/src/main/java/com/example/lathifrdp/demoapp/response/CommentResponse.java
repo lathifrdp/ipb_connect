facdf8d63ea56d023ae2dd8a2d1776da7fab474e
lathifrdp <lathifrdp@gmail.com>
2018-11-09 15:49:03 +0700

    buat respon untuk post comment

---
+++ app/src/main/java/com/example/lathifrdp/demoapp/response/CommentResponse.java
@@ -0,0 +1,33 @@
+package com.example.lathifrdp.demoapp.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CommentResponse {
+
+    @SerializedName("isSuccess")
+    private boolean isSuccess;
+
+    @SerializedName("message")
+    private String message;
+
+    public CommentResponse(boolean isSuccess, String message){
+        this.isSuccess = isSuccess;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+}
