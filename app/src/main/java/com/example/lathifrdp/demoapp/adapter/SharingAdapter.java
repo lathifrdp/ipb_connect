48c2287d7bc9960a34dfc15235bbcec63f14b217
lathifrdp <lathifrdp@gmail.com>
2018-11-14 01:15:35 +0700

    ngasih put string id_sharing

--- app/src/main/java/com/example/lathifrdp/demoapp/adapter/SharingAdapter.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/adapter/SharingAdapter.java
@@ -1,6 +1,11 @@
 package com.example.lathifrdp.demoapp.adapter;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lathifrdp.demoapp.R;
+import com.example.lathifrdp.demoapp.fragment.sharing.DetailSharingFragment;
 import com.example.lathifrdp.demoapp.model.KnowledgeSharing;
 import com.squareup.picasso.Picasso;
 
@@ -87,7 +93,23 @@ public class SharingAdapter extends RecyclerView.Adapter<SharingAdapter.ViewHold
 //                        intent.putExtra("movie_plot", movies.get(pos).getPlot());
 //                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 //                        context.startActivity(intent);
-                        Toast.makeText(v.getContext(), "You clicked " + clickedDataItem.getTitle(), Toast.LENGTH_SHORT).show();
+
+                        Bundle bundle;
+                        bundle = new Bundle();
+                        Fragment newFragment = null;
+                        newFragment = new DetailSharingFragment();
+
+                        bundle.putString("id_sharing",clickedDataItem.getId());
+                        newFragment.setArguments(bundle);
+
+                        AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+                        FragmentTransaction ft = fragmentManager.beginTransaction();
+                        ft.replace(R.id.screen_area, newFragment);
+                        ft.addToBackStack(null);
+                        ft.commit();
+
+                        //Toast.makeText(v.getContext(), "You clicked " + clickedDataItem.getId(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
