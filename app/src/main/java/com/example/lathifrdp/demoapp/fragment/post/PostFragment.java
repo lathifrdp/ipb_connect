f1e712386e3916d673b0a7f98e674a1320cf3311
lathifrdp <lathifrdp@gmail.com>
2018-11-28 22:53:28 +0700

    tambah onclick tiap icon

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/post/PostFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/post/PostFragment.java
@@ -3,15 +3,21 @@ package com.example.lathifrdp.demoapp.fragment.post;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.lathifrdp.demoapp.R;
 
 public class PostFragment extends Fragment{
+
+    RelativeLayout job, event, memories, sharing, group, crowd;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,11 +29,86 @@ public class PostFragment extends Fragment{
         super.onViewCreated(view, savedInstanceState);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Post Activity");
-        view.findViewById(R.id.postFragment).setOnClickListener(new View.OnClickListener() {
+        job = (RelativeLayout) getView().findViewById(R.id.post_job);
+        event = (RelativeLayout) getView().findViewById(R.id.post_event);
+        memories = (RelativeLayout) getView().findViewById(R.id.post_memories);
+        sharing = (RelativeLayout) getView().findViewById(R.id.post_sharing);
+        group = (RelativeLayout) getView().findViewById(R.id.post_group);
+        crowd = (RelativeLayout) getView().findViewById(R.id.post_crowd);
+
+        job.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(),"Post Fragment",Toast.LENGTH_SHORT).show();
+
+                Fragment fragment = null;
+                fragment = new PostJobFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
             }
         });
+
+        event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                fragment = new PostEventFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        memories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                fragment = new PostMemoriesFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        sharing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                fragment = new PostSharingFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                fragment = new PostGroupFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        crowd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                fragment = new PostCrowdFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
     }
 }
