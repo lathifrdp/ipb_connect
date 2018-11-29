f4bd305f81561e7a426b1caa2a81c7e24ada0b21
lathifrdp <lathifrdp@gmail.com>
2018-11-29 20:46:30 +0700

    ubah tampilan memories

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/memories/DetailMemoriesFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/memories/DetailMemoriesFragment.java
@@ -21,13 +21,16 @@ import com.example.lathifrdp.demoapp.api.ApiInterface;
 import com.example.lathifrdp.demoapp.helper.BaseModel;
 import com.example.lathifrdp.demoapp.helper.SessionManager;
 import com.example.lathifrdp.demoapp.model.Comment;
+import com.example.lathifrdp.demoapp.model.Liker;
 import com.example.lathifrdp.demoapp.model.Memory;
 import com.example.lathifrdp.demoapp.response.GetCommentResponse;
 import com.example.lathifrdp.demoapp.response.PostCommentResponse;
+import com.example.lathifrdp.demoapp.response.PostLikeResponse;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -35,15 +38,18 @@ import retrofit2.Response;
 public class DetailMemoriesFragment extends Fragment{
 
     SessionManager sessionManager;
-    TextView caption,comment,judul_komen,namanya;
+    TextView caption,comment,judul_komen,namanya, total_like, nama_capt;
     EditText tulis;
-    ImageView foto, kirim;
+    ImageView foto, kirim, suka;
     String memo, msg;
     Bundle bundle;
     ApiInterface apiService;
     public List<Comment> commentsList;
     private RecyclerView recyclerViewCom;
     CommentList commentAdapter;
+    CircleImageView pp;
+    public boolean stateLike = false;
+    String jumlahlike;
 
     @Nullable
     @Override
@@ -69,6 +75,10 @@ public class DetailMemoriesFragment extends Fragment{
         tulis = (EditText) getView().findViewById(R.id.tulis_komentar);
         kirim = (ImageView) getView().findViewById(R.id.kirim_komentar);
         judul_komen = (TextView) getView().findViewById(R.id.judul_komen);
+        pp = (CircleImageView) getView().findViewById(R.id.foto_posting_memory);
+        suka = (ImageView) getView().findViewById(R.id.like_memories);
+        total_like = (TextView) getView().findViewById(R.id.totalLike_memories);
+        nama_capt = (TextView) getView().findViewById(R.id.nama_caption_memories);
 
         if(bundle != null){
             // handle your code here.
@@ -86,6 +96,40 @@ public class DetailMemoriesFragment extends Fragment{
                 //commentAdapter.notifyDataSetChanged();
                 //loadComment();
                 //tulis.getText().clear();//clears the text
+            }
+        });
+        suka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(stateLike==false) {
+                    postLike();
+                }
+                else if(stateLike==true){
+                    postUnlike();
+                }
+            }
+        });
+    }
+
+    private void loadLike(){
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<Memory> call = apiService.getDetailMemory("JWT "+ sessionManager.getKeyToken(),memo);
+        call.enqueue(new Callback<Memory>() {
+            @Override
+            public void onResponse(Call<Memory> call, final Response<Memory> response) {
+                if (response.isSuccessful()) {
+
+                    Memory memory = response.body();
+                    jumlahlike = memory.getTotalLike()+" likes";
+                    total_like.setText(jumlahlike);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Memory> call, Throwable t) {
+                Toast.makeText(getActivity(), "gagal", Toast.LENGTH_SHORT).show();
+                //mSwipeRefreshLayout.setRefreshing(false);
             }
         });
     }
@@ -108,14 +152,29 @@ public class DetailMemoriesFragment extends Fragment{
 
                     Memory memory = response.body();
                     //commentsList = memory.getComment();
-//                    List<Liker> like = memory.getLiker();
+                    List<Liker> like = memory.getLiker();
+
+                    int i = 0;
+                    //likeSize = Integer.parseInt(ks.getTotalLike());
+                    for(i=0;i<like.size();i++) {
+                        if(like.get(i).getCreatedBy().getId().equals(sessionManager.getKeyId())){
+                            suka.setColorFilter(getContext().getResources().getColor(R.color.merah));
+                            stateLike=true;
+                        }
+                        else{
+                            stateLike=false;
+                        }
+                    }
 //
 //                    commentAdapter = new CommentList(commentsList);
 //                    recyclerViewCom.setAdapter(commentAdapter);
 //                    recyclerViewCom.smoothScrollToPosition(0);
 
+                    jumlahlike = memory.getTotalLike()+" likes";
+                    nama_capt.setText(memory.getUser().getFullName());
                     caption.setText(memory.getCaption());
                     namanya.setText(memory.getUser().getFullName());
+                    total_like.setText(jumlahlike);
 //                    for(int i=0;i<commentsList.size();i++) {
 //                        comment.setText("Comment: " + commentsList.get(i).getCreated());
 //                    }
@@ -126,6 +185,13 @@ public class DetailMemoriesFragment extends Fragment{
                             .placeholder(R.drawable.placegam)
                             .error(R.drawable.logoipb)
                             .into(foto);
+
+                    String urlPP = new BaseModel().getProfileUrl()+memory.getUser().getUserProfile().getPhoto();
+                    Picasso.get()
+                            .load(urlPP)
+                            .placeholder(R.drawable.placegam)
+                            .error(R.drawable.logoipb)
+                            .into(pp);
                 }
             }
 
@@ -172,7 +238,6 @@ public class DetailMemoriesFragment extends Fragment{
         final String value = tulis.getText().toString();
         final String createdBy = sessionManager.getKeyId();
 
-
         Call<PostCommentResponse> ucall = apiService.postComment("JWT "+ sessionManager.getKeyToken(),value,createdBy,memo);
         ucall.enqueue(new Callback<PostCommentResponse>() {
             @Override
@@ -187,9 +252,7 @@ public class DetailMemoriesFragment extends Fragment{
                     }
                     else {
                         msg = cr.getMessage();
-
                         Toast.makeText(getActivity(), cr.getMessage(), Toast.LENGTH_SHORT).show();
-
                     }
                     loadComment();
                     tulis.getText().clear();
@@ -203,5 +266,67 @@ public class DetailMemoriesFragment extends Fragment{
             }
         });
 
+    }
+
+    private void postLike() {
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+        final String createdBy = sessionManager.getKeyId();
+
+        Call<PostLikeResponse> ucall = apiService.postLikeMemories("JWT "+ sessionManager.getKeyToken(),createdBy,memo);
+        ucall.enqueue(new Callback<PostLikeResponse>() {
+            @Override
+            public void onResponse(Call<PostLikeResponse> call, Response<PostLikeResponse> response) {
+
+                if (response.isSuccessful()) {
+                    PostLikeResponse lr = response.body();
+
+                    if(lr.isSuccess()==false ){
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                        suka.setColorFilter(getContext().getResources().getColor(R.color.merah));
+                        stateLike=true;
+                        loadLike();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostLikeResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void postUnlike() {
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+        final String createdBy = sessionManager.getKeyId();
+
+        Call<PostLikeResponse> ucall = apiService.postUnlikeMemories("JWT "+ sessionManager.getKeyToken(),createdBy,memo);
+        ucall.enqueue(new Callback<PostLikeResponse>() {
+            @Override
+            public void onResponse(Call<PostLikeResponse> call, Response<PostLikeResponse> response) {
+
+                if (response.isSuccessful()) {
+                    PostLikeResponse lr = response.body();
+
+                    if(lr.isSuccess()==false ){
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                        suka.setColorFilter(getContext().getResources().getColor(R.color.abu));
+                        stateLike=false;
+                        loadLike();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostLikeResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
