867fe1069fb63d1e465915e734b17b44292a0fb0
lathifrdp <lathifrdp@gmail.com>
2018-12-24 12:46:41 +0700

    ngilangin typo i di belakang kata request

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/crowdfunding/alumni/CrowdRequestFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/crowdfunding/alumni/CrowdRequestFragment.java
@@ -22,7 +22,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class CrowdRequestiFragment extends Fragment{
+public class CrowdRequestFragment extends Fragment{
 
     Button req;
     SessionManager sessionManager;
