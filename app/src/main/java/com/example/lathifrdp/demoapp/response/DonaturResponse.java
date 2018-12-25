6b3fd06adb6f1998d856548fc4027b906a527829
lathifrdp <lathifrdp@gmail.com>
2018-12-25 19:09:13 +0700

    buat respon donatur

---
+++ app/src/main/java/com/example/lathifrdp/demoapp/response/DonaturResponse.java
@@ -0,0 +1,72 @@
+package com.example.lathifrdp.demoapp.response;
+
+import com.example.lathifrdp.demoapp.model.Crowdfunding;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class DonaturResponse {
+    @SerializedName("items")
+    private List<Crowdfunding> crowdfunding;
+
+    @SerializedName("page")
+    private Integer page;
+
+//    @SerializedName("limit")
+//    private Integer limit;
+
+    @SerializedName("total")
+    private Integer total;
+
+    @SerializedName("isSuccess")
+    private boolean isSuccess;
+
+    @SerializedName("message")
+    private String message;
+
+    public DonaturResponse(List<Crowdfunding> crowdfunding, Integer page, Integer total, boolean isSuccess, String message){
+        this.crowdfunding = crowdfunding;
+        this.page = page;
+        this.total = total;
+    }
+
+    public List<Crowdfunding> getCrowdfunding() {
+        return crowdfunding;
+    }
+
+    public void setCrowdfunding(List<Crowdfunding> crowdfunding) {
+        this.crowdfunding = crowdfunding;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+}
