692135bacc4c1130731f7e0918248ff24dcf7006
lathifrdp <lathifrdp@gmail.com>
2019-01-05 16:31:56 +0700

    mengatur input pin per edit text

--- app/src/main/java/com/example/lathifrdp/demoapp/fragment/crowdfunding/alumni/CrowdPinFragment.java
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/crowdfunding/alumni/CrowdPinFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +27,7 @@ import retrofit2.Response;
 
 public class CrowdPinFragment extends Fragment {
     Button pinbtn;
-    EditText pin;
+    EditText pin,pin1,pin2,pin3,pin4,pin5,pin6;
     SessionManager sessionManager;
     ApiInterface apiService;
     ProgressDialog pd;
@@ -42,7 +44,17 @@ public class CrowdPinFragment extends Fragment {
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Crowdfunding");
         sessionManager = new SessionManager(getActivity());
-        pin = (EditText) getView().findViewById(R.id.crowd_pin);
+        //pin = (EditText) getView().findViewById(R.id.crowd_pin);
+        pin1 = (EditText) getView().findViewById(R.id.pin1);
+        pin2 = (EditText) getView().findViewById(R.id.pin2);
+        pin3 = (EditText) getView().findViewById(R.id.pin3);
+        pin4 = (EditText) getView().findViewById(R.id.pin4);
+        pin5 = (EditText) getView().findViewById(R.id.pin5);
+        pin6 = (EditText) getView().findViewById(R.id.pin6);
+
+        tombolNext();
+        pinGeser();
+
         pinbtn = (Button) getView().findViewById(R.id.crowd_btn_pin);
         pinbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,12 +68,15 @@ public class CrowdPinFragment extends Fragment {
         });
     }
     private void postPin(){
-        final String key = pin.getText().toString();
+        //final String key = pin.getText().toString();
         final String createdBy = sessionManager.getKeyId();
+        final String gabungpin = pin1.getText().toString()+pin2.getText().toString()+
+                                 pin3.getText().toString()+pin4.getText().toString()+
+                                 pin5.getText().toString()+pin6.getText().toString();
 
         apiService = ApiClient.getClient().create(ApiInterface.class);
 
-        Call<PinResponse> ucall = apiService.postPin("JWT "+ sessionManager.getKeyToken(),createdBy,key);
+        Call<PinResponse> ucall = apiService.postPin("JWT "+ sessionManager.getKeyToken(),createdBy,gabungpin);
         ucall.enqueue(new Callback<PinResponse>() {
             @Override
             public void onResponse(Call<PinResponse> call, Response<PinResponse> response) {
@@ -72,10 +87,12 @@ public class CrowdPinFragment extends Fragment {
 
                     if(pr.isSuccess()==false ){
                         Toast.makeText(getActivity(), pr.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), gabungpin, Toast.LENGTH_SHORT).show();
                     }
                     else {
                         //Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
                         Toast.makeText(getActivity(), pr.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), gabungpin, Toast.LENGTH_SHORT).show();
                         //sessionManager.updateCrowdfunding("3");
                         Fragment newFragment = null;
                         newFragment = new CrowdAlumniFragment();
@@ -92,6 +109,111 @@ public class CrowdPinFragment extends Fragment {
             public void onFailure(Call<PinResponse> call, Throwable t) {
                 Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
                 pd.dismiss();
+            }
+        });
+    }
+
+    private void tombolNext(){
+        pin1.setNextFocusDownId(pin2.getId());
+        pin2.setNextFocusDownId(pin3.getId());
+        pin3.setNextFocusDownId(pin4.getId());
+        pin4.setNextFocusDownId(pin5.getId());
+        pin5.setNextFocusDownId(pin6.getId());
+    }
+
+    private void pinGeser(){
+        pin1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(pin1.getText().toString().length() == 1){
+                    pin2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        pin2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(pin2.getText().toString().length() == 1){
+                    pin3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        pin3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(pin3.getText().toString().length() == 1){
+                    pin4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        pin4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(pin4.getText().toString().length() == 1){
+                    pin5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        pin5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(pin5.getText().toString().length() == 1){
+                    pin6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
