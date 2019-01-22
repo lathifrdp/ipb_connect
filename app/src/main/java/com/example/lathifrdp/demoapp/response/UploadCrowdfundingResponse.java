95d058d688c6d7f5214372426a8c94608138537a
lathifrdp <lathifrdp@gmail.com>
2019-01-22 14:39:34 +0700

    buat upload crowd response

---
+++ app/src/main/java/com/example/lathifrdp/demoapp/response/UploadCrowdfundingResponse.java
@@ -0,0 +1,32 @@
+package com.example.lathifrdp.demoapp.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UploadCrowdfundingResponse {
+    @SerializedName("isSuccess")
+    private boolean isSuccess;
+
+    @SerializedName("message")
+    private String message;
+
+    public UploadCrowdfundingResponse(boolean isSuccess, String message){
+        this.isSuccess = isSuccess;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+}
