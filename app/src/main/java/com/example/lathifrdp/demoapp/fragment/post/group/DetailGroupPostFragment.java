5a7f0db0b4f539dfd44cbb7c45289431efb5f3f9
lathifrdp <lathifrdp@gmail.com>
2019-02-04 17:10:49 +0700

    buat detail post grup diskusi

---
+++ app/src/main/java/com/example/lathifrdp/demoapp/fragment/post/group/DetailGroupPostFragment.java
@@ -0,0 +1,360 @@
+package com.example.lathifrdp.demoapp.fragment.post.group;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lathifrdp.demoapp.R;
+import com.example.lathifrdp.demoapp.adapter.DiscussionList;
+import com.example.lathifrdp.demoapp.api.ApiClient;
+import com.example.lathifrdp.demoapp.api.ApiInterface;
+import com.example.lathifrdp.demoapp.helper.BaseModel;
+import com.example.lathifrdp.demoapp.helper.SessionManager;
+import com.example.lathifrdp.demoapp.model.Comment;
+import com.example.lathifrdp.demoapp.model.GroupDiscussion;
+import com.example.lathifrdp.demoapp.model.Liker;
+import com.example.lathifrdp.demoapp.response.DeleteResponse;
+import com.example.lathifrdp.demoapp.response.GetCommentResponse;
+import com.example.lathifrdp.demoapp.response.PostCommentResponse;
+import com.example.lathifrdp.demoapp.response.PostLikeResponse;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailGroupPostFragment extends Fragment {
+
+    SessionManager sessionManager;
+    Bundle bundle;
+    ApiInterface apiService;
+    public List<Comment> commentsList;
+    private RecyclerView rvDiscuss;
+    DiscussionList discussionAdapter;
+    public String group_id;
+    TextView nama_posting, judul, deskripsi, judul_jawaban, total_like;
+    ImageView suka;
+    CircleImageView pp;
+    public boolean stateLike = false;
+    String jumlahlike;
+    FloatingActionButton fab_ubah, fab_hapus;
+    ProgressDialog pd;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail_group_post,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Diskusi");
+        sessionManager = new SessionManager(getActivity());
+
+        rvDiscuss= (RecyclerView) getView().findViewById(R.id.jawaban_discussion);
+        rvDiscuss.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvDiscuss.setHasFixedSize(true);
+
+        nama_posting = (TextView) getView().findViewById(R.id.nama_posting_group);
+        judul = (TextView) getView().findViewById(R.id.judul_group);
+        deskripsi = (TextView) getView().findViewById(R.id.deskripsi_group);
+        judul_jawaban = (TextView) getView().findViewById(R.id.jawaban_group);
+        pp = (CircleImageView) getView().findViewById(R.id.foto_posting_group);
+        suka = (ImageView) getView().findViewById(R.id.like_group);
+        total_like = (TextView) getView().findViewById(R.id.totalLike_group);
+        fab_ubah = (FloatingActionButton) getView().findViewById(R.id.edit_group_post);
+        fab_hapus = (FloatingActionButton) getView().findViewById(R.id.delete_group_post);
+
+        bundle = this.getArguments();
+        if(bundle != null){
+            // handle your code here.
+            group_id = bundle.getString("id_group");
+        }
+        else {
+            Toast.makeText(getActivity(), "gagal bos", Toast.LENGTH_SHORT).show();
+        }
+
+        loadDataGroup();
+        loadDiscuss();
+
+        fab_ubah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment createFragment = null;
+                createFragment = new EditGroupFragment();
+                createFragment.setArguments(bundle);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.screen_area, createFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                //Toast.makeText(getActivity(), "ubah", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fab_hapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                alertDialog.setTitle("Hapus");
+                alertDialog.setMessage("Apakah anda yakin untuk menghapus data ini?");
+
+                alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        alertDialog.dismiss();
+                        pd = new ProgressDialog(getActivity());
+                        pd.setMessage("Menghapus data...");
+                        pd.setCancelable(false);
+                        pd.show();
+                        deleteGroup();
+                    }
+                });
+
+                alertDialog.setButton(Dialog.BUTTON_NEGATIVE, "Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                // Showing Alert Message
+                alertDialog.show();
+            }
+        });
+
+        suka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(stateLike==false) {
+                    postLike();
+                }
+                else if(stateLike==true){
+                    postUnlike();
+                }
+            }
+        });
+    }
+
+    private void loadLike(){
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<GroupDiscussion> call = apiService.getDetailGroup("JWT "+ sessionManager.getKeyToken(),group_id);
+        call.enqueue(new Callback<GroupDiscussion>() {
+            @Override
+            public void onResponse(Call<GroupDiscussion> call, final Response<GroupDiscussion> response) {
+                if (response.isSuccessful()) {
+
+                    GroupDiscussion gd = response.body();
+                    jumlahlike = gd.getTotalLike()+" orang menyukai";
+                    total_like.setText(jumlahlike);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GroupDiscussion> call, Throwable t) {
+                Toast.makeText(getActivity(), "gagal", Toast.LENGTH_SHORT).show();
+                //mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void postLike() {
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+        final String createdBy = sessionManager.getKeyId();
+
+        Call<PostLikeResponse> ucall = apiService.postLikeGroup("JWT "+ sessionManager.getKeyToken(),createdBy,group_id);
+        ucall.enqueue(new Callback<PostLikeResponse>() {
+            @Override
+            public void onResponse(Call<PostLikeResponse> call, Response<PostLikeResponse> response) {
+
+                if (response.isSuccessful()) {
+                    PostLikeResponse lr = response.body();
+
+                    if(lr.isSuccess()==false ){
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                        suka.setColorFilter(getContext().getResources().getColor(R.color.merah));
+                        stateLike=true;
+                        loadLike();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostLikeResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void postUnlike() {
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+        final String createdBy = sessionManager.getKeyId();
+
+        Call<PostLikeResponse> ucall = apiService.postUnlikeGroup("JWT "+ sessionManager.getKeyToken(),createdBy,group_id);
+        ucall.enqueue(new Callback<PostLikeResponse>() {
+            @Override
+            public void onResponse(Call<PostLikeResponse> call, Response<PostLikeResponse> response) {
+
+                if (response.isSuccessful()) {
+                    PostLikeResponse lr = response.body();
+
+                    if(lr.isSuccess()==false ){
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getActivity(), lr.getMessage(), Toast.LENGTH_SHORT).show();
+                        suka.setColorFilter(getContext().getResources().getColor(R.color.abu));
+                        stateLike=false;
+                        loadLike();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostLikeResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadDataGroup(){
+
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<GroupDiscussion> call = apiService.getDetailGroup("JWT "+ sessionManager.getKeyToken(),group_id);
+        call.enqueue(new Callback<GroupDiscussion>() {
+            @Override
+            public void onResponse(Call<GroupDiscussion> call, final Response<GroupDiscussion> response) {
+                //mSwipeRefreshLayout.setRefreshing(false);
+                if (response.isSuccessful()) {
+
+                    GroupDiscussion gd = response.body();
+                    List<Liker> like = gd.getLiker();
+
+                    int i = 0;
+                    for(i=0;i<like.size();i++) {
+                        if(like.get(i).getCreatedBy().getId().equals(sessionManager.getKeyId())){
+                            suka.setColorFilter(getContext().getResources().getColor(R.color.merah));
+                            stateLike=true;
+                        }
+                        else{
+                            stateLike=false;
+                        }
+                    }
+
+                    nama_posting.setText(gd.getCreatedBy().getFullName());
+                    judul.setText(gd.getTitle());
+                    deskripsi.setText(gd.getDescription());
+                    jumlahlike = gd.getTotalLike()+" orang menyukai";
+                    total_like.setText(jumlahlike);
+
+                    String urlPP = new BaseModel().getProfileUrl()+gd.getCreatedBy().getUserProfile().getPhoto();
+                    Picasso.get()
+                            .load(urlPP)
+                            .placeholder(R.drawable.placegam)
+                            .error(R.drawable.placeholdergambar)
+                            .into(pp);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GroupDiscussion> call, Throwable t) {
+                Toast.makeText(getActivity(), "gagal", Toast.LENGTH_SHORT).show();
+                //mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void loadDiscuss(){
+
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<GetCommentResponse> call = apiService.getDiscussion("JWT "+ sessionManager.getKeyToken(),group_id);
+        call.enqueue(new Callback<GetCommentResponse>() {
+            @Override
+            public void onResponse(Call<GetCommentResponse> call, final Response<GetCommentResponse> response) {
+                //mSwipeRefreshLayout.setRefreshing(false);
+                if (response.isSuccessful()) {
+
+                    GetCommentResponse cgr = response.body();
+                    commentsList = cgr.getComments();
+                    judul_jawaban.setText(commentsList.size()+" Jawaban");
+                    discussionAdapter = new DiscussionList(commentsList);
+                    rvDiscuss.setAdapter(discussionAdapter);
+                    rvDiscuss.smoothScrollToPosition(0);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetCommentResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "gagal", Toast.LENGTH_SHORT).show();
+                //mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    }
+
+    private void deleteGroup(){
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<DeleteResponse> ucall = apiService.deleteGroup("JWT "+ sessionManager.getKeyToken(),group_id);
+        ucall.enqueue(new Callback<DeleteResponse>() {
+            @Override
+            public void onResponse(Call<DeleteResponse> call, Response<DeleteResponse> response) {
+
+                if (response.isSuccessful()) {
+
+                    DeleteResponse mr = response.body();
+
+                    if(mr.isSuccess()==false ){
+                        Snackbar.make(getView(), mr.getMessage(), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                    else {
+                        //Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+                        Snackbar.make(getView(), mr.getMessage(), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        Fragment newFragment = null;
+                        newFragment = new PostGroupFragment();
+                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        transaction.replace(R.id.screen_area, newFragment);
+                        transaction.addToBackStack(null);
+                        transaction.commit();
+                    }
+                    pd.dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DeleteResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Mohon maaf sedang terjadi gangguan", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        });
+    }
+}
